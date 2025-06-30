33de51e8aa8b511cddde2a2015460bdbf2a6027e
S19102002 <139078240+S19102002@users.noreply.github.com>
2025-07-01 03:51:27 +0530

    Update AdminController.java

--- app/src/main/java/com/project/back_end/controllers/AdminController.java
+++ app/src/main/java/com/project/back_end/controllers/AdminController.java
@@ -1,5 +1,5 @@
 
-package com.project.back_end.controllers;
+/*package com.project.back_end.controllers;
 
 public class AdminController {
 
@@ -23,5 +23,33 @@ public class AdminController {
 
 
 
+}
+*/
+package com.project.back_end.controllers;
+
+import com.project.back_end.model.Admin;
+import com.project.back_end.services.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController // 1. Mark this class as a REST controller
+@RequestMapping("${api.path}admin") // 1. Base path for all admin endpoints
+public class AdminController {
+
+    private final Service service; // 2. Injected service
+
+    @Autowired
+    public AdminController(Service service) {
+        this.service = service;
+    }
+
+    // 3. Admin Login Endpoint
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> adminLogin(@RequestBody Admin admin) {
+        return service.validateAdmin(admin); // Delegates to service logic
+    }
 }
 
