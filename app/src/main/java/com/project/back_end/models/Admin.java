13579e3375219714232cfe6764dda87422fde80a
S19102002 <139078240+S19102002@users.noreply.github.com>
2025-07-01 13:39:19 +0530

    Update Admin.java

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -35,7 +35,7 @@ public class Admin {
 //    - Standard getter and setter methods are provided for accessing and modifying the fields.
 
 }*/
-package com.project.back_end.models;
+/*package com.project.back_end.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
@@ -95,5 +95,37 @@ public class Admin {
     public void setPassword(String password) {
         this.password = password;
     }
+}
+*/
+package com.project_back_end.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    // Constructors
+    public Admin() {}
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
 
